1244efdff8100a243c0e0355108c8ddfb4dbb20e
Vung Nguyen <vungnguyenn1001@gmail.com>
2024-03-23 19:04:39 +0700

    update dynamic overlay message

--- src/models/model.overlay.message.js
+++ src/models/model.overlay.message.js
@@ -5,12 +5,16 @@ const overlayMessageSchema = mongoose.Schema(
         customer_id: { type: mongoose.Schema.Types.ObjectId, ref: "customers", required: true, },
         status: { type: Number, required: true },
         notification: { type: String, required: true },
+        colors_gradient: [{
+            type: String
+        }],
         image: { type: String, required: true },
         title_image: { type: String, required: true },
         content_image: { type: String, required: true },
         title: { type: String, required: true },
         content: { type: String, required: true },
         text_action: { type: String, required: true },
+        color_action: { type: String, required: false },
         action: { type: String, required: false },
         created_at: { type: String, required: true },
     },
